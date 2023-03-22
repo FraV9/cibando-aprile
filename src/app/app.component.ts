823af78008b31990d81190b928897b13f70ea77c
Francesco <francescov445@gmail.com>
2023-03-22 16:31:56 +0100

    secondo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'cibando-aprile';
+
+  images = [
+    {id: 1,
+    label: 'spaghetti al pomorodoro'},
+    {id: 2,
+      label: 'tagliata di manzo'},
+    {id: 3,
+      label: 'tiramisù'}
+  ];
+
+  percorso = "../assets/images/carousel-";
+
+  colore = "blue";
 }
+
+
